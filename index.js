b06cde0694595b95836d8b95c3f540200d581bdf
Maham Babar <mahambabar11@gmail.com>
2022-07-22 05:33:23 -0700

    my web

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let num = prompt("Enter Table");
+let num = +prompt("Enter Table");
+if (isNaN(num)) {
+    alert("invalid entry.please enter a number!")
+}
+else{
 let i = 0;
 document.write('<table border ="10" cellspacing= "10">');
 for (i = 0; i <= 10; i++) {
     document.write("<tr><td bgcolor= white >" + num + " x " + i + " = " + num * i + "</td></tr>");
 }
 document.write("</table>");
+}
